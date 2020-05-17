0fbf648d2cabdc91f888e8cc68501599d38b97be
Amit Choudhary <53244875+AmitChoudhary1392@users.noreply.github.com>
2020-05-17 15:58:40 -0400

    adding multi-page web scraper

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+'''*************************** SINGLE PAGE WEB SCRAPER *****************************************'''
 #url of website to query
 url="http://quotes.toscrape.com/"
 
@@ -31,4 +32,45 @@ for i in range(0, len(quotes)):
 
 
 
+'''*************************** MULTI-PAGE WEB SCRAPER *****************************************'''
+#url of website to query
+url2= "https://scrapingclub.com/exercise/list_basic/?page=1"
 
+def web_scraper(url):
+    #GET query to get the response
+    response_multi= requests.get(url)
+    #print(response.text)           #see output of the GET request
+
+    #parse the HTML object using beautifulsoup and lxml
+    soup_multi=BeautifulSoup(response_multi.text,'lxml')
+
+    #inspect the HTML tags to identify what to scrape
+    items= soup_multi.find_all('div', class_="col-lg-4 col-md-6 mb-4")
+
+    count=1
+    #grab the title and price tag from the items
+    for i in items:
+        item_name= i.find('h4', class_="card-title").text
+        item_price= i.find("h5").text
+
+        print('%s) Price: %s, Item Name: %s' % (count, item_price, item_name))
+        counter+=1
+
+web_scraper(url2)
+# inspect the html page for page navigation
+pages= soup_multi.find('ul', class_="pagination")
+#store the individaul page links in a list
+urls=[]
+links= pages.find_all('a', class_='page-link')
+
+for link in links:
+    page_num= int(link.text) if link.text.isdigit() else None           # page number contains integer and strings. selecting the string will duplicate the result as it will take to the same page.\n have to put a conditional to select only the integer page number
+    
+    if page_num !=None:
+        x= link.get('href')
+        urls.append(x)
+        new_url= url2+ x        #build a new Url for each page
+
+    web_scraper(new_url)        #use the function to display results on each page
+
+    print("*"*9 +" Next Page" + "*"*9)
